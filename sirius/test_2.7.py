843fdc89008c99a15043de597ebb97b53c67adb4
Alex Zorg <azorg@mail.ru>
2021-02-17 17:29:48 +0300

    add sirius

---
+++ sirius/test_2.7.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s = input("Input number: ")
+num = int(s)
+print("The next number for the number ", num, " is ", num + 1, ".", sep='')
+print("The previous number for the number ", num, " is ", num -1, ".", sep='')
+
